605628b0994c9b589b8c04e279b68fa405b6386c
Tomas Radvansky <tomas@rdev.co.nz>
2025-10-27 06:59:57 +1300

    add di

--- lib/flutter_linkme_sdk.dart
+++ lib/flutter_linkme_sdk.dart
@@ -7,7 +7,45 @@ export 'src/models.dart';
 
 class LinkMe {
   LinkMe({FlutterLinkmeSdkPlatform? platform})
-      : _platform = platform ?? FlutterLinkmeSdkPlatform.instance;
+    : _platform = platform ?? FlutterLinkmeSdkPlatform.instance;
+
+  final FlutterLinkmeSdkPlatform _platform;
+
+  Stream<LinkMePayload> get onLink => _platform.onLink;
+
+  Future<void> configure(LinkMeConfig config) {
+    return _platform.configure(config);
+  }
+
+  Future<LinkMePayload?> getInitialLink() {
+    return _platform.getInitialLink();
+  }
+
+  Future<LinkMePayload?> claimDeferredIfAvailable() {
+    return _platform.claimDeferredIfAvailable();
+  }
+
+  Future<void> setUserId(String? userId) {
+    return _platform.setUserId(userId);
+  }
+
+  Future<void> setAdvertisingConsent(bool granted) {
+    return _platform.setAdvertisingConsent(granted);
+  }
+
+  Future<void> track(String event, {Map<String, dynamic>? properties}) {
+    return _platform.track(event, properties: properties);
+  }
+
+  Future<void> setReady() {
+    return _platform.setReady();
+  }
+}
+
+/// Instance-based client for DI/testing, mirroring Node SDK style
+class LinkMeClient {
+  LinkMeClient({FlutterLinkmeSdkPlatform? platform})
+    : _platform = platform ?? FlutterLinkmeSdkPlatform.instance;
 
   final FlutterLinkmeSdkPlatform _platform;
 
